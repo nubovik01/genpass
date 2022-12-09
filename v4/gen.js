91ad996ddb5246fdb736d3cbe9c88005a118a95f
Nikita <50026919+Nubovik01@users.noreply.github.com>
2022-12-09 15:38:34 +0300

    v4 - Added special symbols in generator

---
+++ v4/gen.js
@@ -0,0 +1,26 @@
+let config = require("../config.json");
+var list = "";
+if (config.EnglishLetters) list += "abcdefghijklmnopqrstuvwxyz";
+if (config.EnglishCapitalLetters) list += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if (config.RussianLetters) list += "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+if (config.RussianCapitalLetters) list += "АБВГДЕЁЖЗЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+if (config.numbers) list += "0123456789";
+if(config.symbols) list+="!@#$%^&*()-_=+;:,./?\|`~[]{}";
+
+if(config.lenght==0 || !config.lenght) return console.log("Choose length a password in config");
+
+if (
+  !config.EnglishLetters &&
+  !config.EnglishCapitalLetters &&
+  !config.RussianLetters &&
+  !config.RussianCapitalLetters &&
+  !config.numbers &&
+  !config.symbols
+) return console.log("Choose something in config for generate password");
+
+res = '';
+for (var i = 0, n = list.length; i < config.lenght; ++i) {
+  res += list.charAt(Math.floor(Math.random() * n));
+};
+
+return console.log(res);
