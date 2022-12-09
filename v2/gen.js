53432ec29b1f7b5a056e2e6dd02526931f365e53
Nikita <50026919+Nubovik01@users.noreply.github.com>
2022-12-09 16:06:19 +0300

    v2 - changing variable names

--- v2/gen.js
+++ v2/gen.js
@@ -1,13 +1,13 @@
 let config = require("../config.json");
 var list = "abcdefghijklmnopqrstuvwxyz";
-if(config.numbers) list+="0123456789";
 if(config.EnglishCapitalLetters) list+="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+if(config.numbers) list+="0123456789";
 
 if(config.lenght==0 || !config.lenght) return console.log("Choose length a password in config");
 
-res = '';
-for (var i = 0, n = list.length; i < config.lenght; ++i) {
-  res += list.charAt(Math.floor(Math.random() * n));
+result = '';
+for (var index = 0, n = list.length; index < config.lenght; ++index) {
+  result += list.charAt(Math.floor(Math.random() * n));
 };
 
-return console.log(res);
+return console.log(result);
