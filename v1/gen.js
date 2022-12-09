791bb1be4f6c792919fc665ddd3720b9f6b71877
Nikita <50026919+Nubovik01@users.noreply.github.com>
2022-12-09 16:06:06 +0300

    v1 - changing variable names

--- v1/gen.js
+++ v1/gen.js
@@ -3,9 +3,9 @@ var list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 if(config.lenght==0 || !config.lenght) return console.log("Choose length a password in config");
 
-res = '';
-for (var i = 0, n = list.length; i < config.lenght; ++i) {
-  res += list.charAt(Math.floor(Math.random() * n));
+result = '';
+for (var index = 0, n = list.length; index < config.lenght; ++index) {
+  result += list.charAt(Math.floor(Math.random() * n));
 };
 
-return console.log(res);
+return console.log(result);
